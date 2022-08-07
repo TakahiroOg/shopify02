15f19588274718a57c7ff2cb6f3a45009bb9c50f
TakahiroOg <boroney1@gmail.com>
2022-08-07 10:26:12 +0900

    Finish

--- assets/global.js
+++ assets/global.js
@@ -1150,3 +1150,19 @@ $(document).ready(function () {
     $("#" + tab_id).addClass("current-tab");
   });
 });
+
+var acc = document.getElementsByClassName("toggler");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
